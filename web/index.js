d6eb019a36fc9f15ba085a5e1aee88e2caf24cf8
Gavin Morrisseau <81901485+gavinmorrisseau@users.noreply.github.com>
2023-12-10 15:58:46 -0600

    Create gpt-for-web.py, organize, update site.

--- web/index.js
+++ web/index.js
@@ -1,31 +1,7 @@
-// Import required modules
-const express = require('express');
-const markdown = require('markdown-it');
-
-// Create an instance of Express
-const app = express();
-
-// Create an instance of Markdown parser
-const md = markdown();
-
-// Define routes
-app.get('/', (req, res) => {
-    const markdownText = `
-        # Hello, world!
-        
-        This is a **Markdown** block.
-        
-        - Item 1
-        - Item 2
-        - Item 3
-    `;
-    
-    const html = md.render(markdownText);
-    res.send(html);
-});
-
-// Start the server
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+function runPythonScript() {
+    // Get the path to the Python script.
+    var pythonScriptPath = "../src/GPT.py";
+  
+    // Run the Python script.
+    subprocess.run(["python", pythonScriptPath]);
+  }
