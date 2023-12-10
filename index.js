4beb541ce9fa9e8a510cde7e56a43f84cfe64b20
Gavin Morrisseau <81901485+gavinmorrisseau@users.noreply.github.com>
2023-12-10 22:39:06 +0000

    Move /web contents to ~/

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function runPythonScript() {
+function runGPT() {
     // Get the path to the Python script.
-    var pythonScriptPath = "../src/GPT.py";
+    var pythonScriptPath = "/src/GPT.py";
   
     // Run the Python script.
     subprocess.run(["python", pythonScriptPath]);
