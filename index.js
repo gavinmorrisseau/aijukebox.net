05d0d81fdf53c8a85f49d278d4dee3b50731de00
Gavin Morrisseau <81901485+gavinmorrisseau@users.noreply.github.com>
2023-12-11 01:46:27 +0000

    Jquery

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
 
-function runGPT() {
-    // Get the path to the Python script.
-    var pythonScriptPath = "main.py";
-  
-    // Run the Python script.
-    subprocess.run(["python", pythonScriptPath]);
+function runGPT(input) {
+    var jqXHR = $.ajax({
+        type: "POST",
+        url: "/main.py",
+        async: false,
+        data: { param: input }
+    });
+
+    return jqXHR.responseText;
 }
+
+// do something with the response
+response= runPyScript('data to process');
+console.log(response);
