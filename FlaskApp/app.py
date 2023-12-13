c4e9b53fa23184110e6c4ce3f4f5ef54a9d92f99
Gavin Morrisseau <81901485+gavinmorrisseau@users.noreply.github.com>
2023-12-13 08:39:23 +0000

    Minor Style Changes

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -3,48 +3,44 @@ from openai import OpenAI
 import time
 import json
 import sys
-
 import traceback
+
+placeholder_response = {'1': {'artist': '', 'track': ''}, '2': {'artist': '', 'track': ''}, '3': {'artist': '', 'track': ''}, '4': {'artist': '', 'track': ''}, '5': {'artist': '', 'track': ''}} 
 
 app = Flask(__name__)
 
-placeholder = "type anything here ..."
-
-''' RETURNS CHAT GPT'S TO A QUESTION IN JSON FORMAT'''
+''' RETURNS CHAT GPT'S TO A QUESTION IN FORMATTED JSON OBJECT'''
 def run_GPT(question):
-    global placeholder
+    global placeholder_response
     print("RUNNING GPT")
     print("QUESTION: " + question)
-    if(question not in ["",None,'']):
-        placeholder = question
 
-        client = OpenAI(organization='org-1VoooiSLTr711Aax6i6A5nUT',)
-        thread = client.beta.threads.create()
-        assistant_id = "asst_Z8EJORrugfSAMUgxKXWNAaXw"
+    client = OpenAI(organization='org-1VoooiSLTr711Aax6i6A5nUT',)
+    thread = client.beta.threads.create()
+    assistant_id = "asst_Z8EJORrugfSAMUgxKXWNAaXw"
 
-        message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=question,
-        )   
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant_id,
-        )
-        run = wait_on_run(run, client, thread)
-        messages = client.beta.threads.messages.list(thread_id=thread.id)
-        message_string = messages.data[0].content[0].text.value
-        message_parsed = "FAILED"
-        try:
-            message_parsed = json.loads(message_string)
-        except json.decoder.JSONDecodeError:
-            print(f'FAILED: \n{message}')
-        
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=question,
+    )   
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant_id,
+    )
+    run = wait_on_run(run, client, thread)
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    message_string = messages.data[0].content[0].text.value
+    print(message_string)
+    message_parsed = "FAILED"
+    try:
+        message_parsed = json.loads(message_string)
         print("ANSWER: " + str(message_parsed))
         print("COMPLETED GPT")
         return(message_parsed)
-    print("CANCELLED GPT")
-    return ""
+    except json.decoder.JSONDecodeError:
+        print("FAILED! placeholder_response used")
+        return(placeholder_response)
 
 def wait_on_run(run, client, thread):
     while run.status in ["queued","in_progress"]:
@@ -57,6 +53,7 @@ def wait_on_run(run, client, thread):
 
 @app.route('/', methods = ['POST','GET'])
 def index():
+    global placeholder_response
     # if request.method == 'GET':
     #     pass
     question = ''
@@ -67,7 +64,7 @@ def index():
         if(question not in ["",None]):
             message_parsed = run_GPT(question)
         else:
-            message_parsed = ''
+            message_parsed = placeholder_response
         #print("BEHIND MSG PARSED: " + str(message_parsed))
     return render_template("index.html", question=question, message_parsed=message_parsed)
 
